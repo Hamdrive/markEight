aa1628730d720f1584bad671eaa95eab46a7fb7e
ham0Err <hamza@peerlist.io>
2021-09-04 13:47:13 +0530

    [feat] basic structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 function App(){
   return (
     <div>
-
+      <div className="navbar">
+        <h1>Know your Flag Emojis!</h1>
+      </div>
+      <div className="main">
+        <div className="desc">
+          Enter your flag emoji to know which country it belongs to!
+          <br/> Else choose one of the popular flags below
+        </div>
+        <div className="emoji-input">
+          <input></input>
+        </div>
+        <div className="premade-list"></div>
+      </div>
     </div>
   )
 }
